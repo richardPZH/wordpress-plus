994f4a3daee58bfac5184869e908610a7a6699b6
pzh <pengzhenghong107@gmail.com>
2015-12-02 14:45:20 +0800

    update the themes and style
    update the themes and style

--- wp-config.php
+++ wp-config.php
@@ -15,13 +15,13 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'pengzhen_wordpress');
 
 /** MySQL数据库用户名 */
-define('DB_USER', 'xxx');
+define('DB_USER', 'pengzhen_pzh');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', 'xxx');
+define('DB_PASSWORD', 'pzh@107Lab');
 
 /** MySQL主机 */
 define('DB_HOST', 'localhost');
